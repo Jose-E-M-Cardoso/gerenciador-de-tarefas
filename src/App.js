76a0af173b79499ca90126f15fbccbe8f983d6b7
Jose-E-M-Cardoso <joseedu98@hotmail.com.br>
2024-10-11 18:53:59 -0300

    feat(p1-pdmn): exibe lista real

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TarefaEntrada from './TarefaEntrada';
+import TarefaLista from './TarefaLista';
 
 function App() {
+  const [tarefas, setTarefas] = useState([]);
+
+  const adicionarTarefa = (descricao) => {
+    setTarefas([...tarefas, descricao]);
+  };
+
   return (
-    <div className="container">
-      <h1 className="text-center">Hello, tarefas</h1>
+    <div className="container d-flex flex-column align-items-center">
+      <h1 className="display-4 text-center mt-4">Gerenciador de Tarefas</h1>
+      <div className="col-12 col-md-8 col-lg-6">
+        <TarefaEntrada adicionarTarefa={adicionarTarefa} />
+        <TarefaLista tarefas={tarefas} />
+      </div>
     </div>
   );
 }
